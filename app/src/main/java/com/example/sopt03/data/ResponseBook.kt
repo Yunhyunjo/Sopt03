2cb5f4e91fcffb1cfca14197251134ef0a174652
Yunhyunjo <37900920+Yunhyunjo@users.noreply.github.com>
2020-05-22 15:21:50 +0900

    Delete ResponseBook.kt

--- app/src/main/java/com/example/sopt03/data/ResponseBook.kt
+++
@@ -1,9 +0,0 @@
-package com.example.sopt03.data
-
-data class ResponseBook (
-    val title : String,
-    val contents : String,
-    val thumbnail : String,
-    val total_count : Int,
-    val success : Int
-)
